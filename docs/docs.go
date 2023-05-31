e5210c4c3664dab03135bc6e847b66b76f01c2a5
daniferdinandall <daniferdinandall@gmail.com>
2023-05-31 14:21:29 +0700

    add docs insert presensi

--- docs/docs.go
+++ docs/docs.go
@@ -20,6 +20,46 @@ const docTemplate = `{
     "host": "{{.Host}}",
     "basePath": "{{.BasePath}}",
     "paths": {
+        "/ins": {
+            "post": {
+                "description": "Input data presensi.",
+                "consumes": [
+                    "application/json"
+                ],
+                "produces": [
+                    "application/json"
+                ],
+                "tags": [
+                    "Presensi"
+                ],
+                "summary": "Insert data presensi.",
+                "parameters": [
+                    {
+                        "description": "Payload Body [RAW]",
+                        "name": "request",
+                        "in": "body",
+                        "required": true,
+                        "schema": {
+                            "$ref": "#/definitions/controller.Presensi"
+                        }
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "OK",
+                        "schema": {
+                            "$ref": "#/definitions/controller.Presensi"
+                        }
+                    },
+                    "400": {
+                        "description": "Bad Request"
+                    },
+                    "500": {
+                        "description": "Internal Server Error"
+                    }
+                }
+            }
+        },
         "/presensi": {
             "get": {
                 "description": "Mengambil semua data presensi.",
@@ -90,28 +130,43 @@ const docTemplate = `{
             "type": "object",
             "properties": {
                 "durasi": {
-                    "type": "integer"
+                    "type": "integer",
+                    "example": 8
                 },
                 "gmt": {
-                    "type": "integer"
+                    "type": "integer",
+                    "example": 7
                 },
                 "hari": {
                     "type": "array",
                     "items": {
                         "type": "string"
-                    }
+                    },
+                    "example": [
+                        "Senin",
+                        "Selasa",
+                        "Rabu",
+                        "Kamis",
+                        "Jumat",
+                        "Sabtu",
+                        "Minggu"
+                    ]
                 },
                 "jam_keluar": {
-                    "type": "string"
+                    "type": "string",
+                    "example": "16:00"
                 },
                 "jam_masuk": {
-                    "type": "string"
+                    "type": "string",
+                    "example": "08:00"
                 },
                 "piket_tim": {
-                    "type": "string"
+                    "type": "string",
+                    "example": "Piket Z"
                 },
                 "shift": {
-                    "type": "integer"
+                    "type": "integer",
+                    "example": 2
                 }
             }
         },
@@ -119,16 +174,27 @@ const docTemplate = `{
             "type": "object",
             "properties": {
                 "_id": {
-                    "type": "string"
+                    "type": "string",
+                    "example": "123456789"
                 },
                 "hari_kerja": {
                     "type": "array",
                     "items": {
                         "type": "string"
-                    }
+                    },
+                    "example": [
+                        "Senin",
+                        "Selasa",
+                        "Rabu",
+                        "Kamis",
+                        "Jumat",
+                        "Sabtu",
+                        "Minggu"
+                    ]
                 },
                 "jabatan": {
-                    "type": "string"
+                    "type": "string",
+                    "example": "Anonymous"
                 },
                 "jam_kerja": {
                     "type": "array",
@@ -137,10 +203,12 @@ const docTemplate = `{
                     }
                 },
                 "nama": {
-                    "type": "string"
+                    "type": "string",
+                    "example": "Tes Swagger"
                 },
                 "phone_number": {
-                    "type": "string"
+                    "type": "string",
+                    "example": "08123456789"
                 }
             }
         },
@@ -148,26 +216,32 @@ const docTemplate = `{
             "type": "object",
             "properties": {
                 "_id": {
-                    "type": "string"
+                    "type": "string",
+                    "example": "123456789"
                 },
                 "biodata": {
                     "$ref": "#/definitions/controller.Karyawan"
                 },
                 "checkin": {
                     "description": "Datetime     primitive.DateTime ` + "`" + `bson:\"datetime,omitempty\" json:\"datetime,omitempty\"` + "`" + `",
-                    "type": "string"
+                    "type": "string",
+                    "example": "MASUK"
                 },
                 "latitude": {
-                    "type": "number"
+                    "type": "number",
+                    "example": 123.11
                 },
                 "location": {
-                    "type": "string"
+                    "type": "string",
+                    "example": "Bandung"
                 },
                 "longitude": {
-                    "type": "number"
+                    "type": "number",
+                    "example": 123.11
                 },
                 "phone_number": {
-                    "type": "string"
+                    "type": "string",
+                    "example": "08123456789"
                 }
             }
         }
